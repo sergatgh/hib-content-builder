ef114d40217f95ed50dd6060b5cb51f758e601fd
Sergey Solomentsev <tracer.nokia@gmail.com>
2023-12-18 00:54:07 -0500

    First steps: we need to visualize the JSON.

--- src/pages/ContentBuilder.tsx
+++ src/pages/ContentBuilder.tsx
@@ -1,15 +1,59 @@
-import React from 'react';
+import React from "react";
 
-function ContentBuilder() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Content Builder.
-        </p>
-      </header>
-    </div>
-  );
+class CBProps {
+
+}
+
+class CBState {
+  name: string = "";
+}
+
+class ContentBuilder extends React.Component<CBProps, CBState> {
+  constructor(props: {} | Readonly<{}>) {
+    super(props);
+    this.state = { name: "Hello World" };
+  }
+
+  nameChange(e: React.ChangeEvent<HTMLInputElement>) {
+    this.setState({ name: e.target.value });
+  }
+
+  render() {
+    let result = `[
+    {
+        "title": "${this.state.name}",
+        "icon": "transform",
+        "category": "Operators",
+      }
+    ]`;
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>Content Builder.</p>
+
+          <fieldset>
+            <legend>Name</legend>
+            <input
+              type="text"
+              value={this.state.name}
+              onChange={(e) => this.nameChange(e)}
+            ></input>
+          </fieldset>
+
+          <fieldset>
+            <legend>Show content</legend>
+            <textarea>asd</textarea>
+          </fieldset>
+
+          <fieldset>
+            <legend>Result</legend>
+            <code>{result}</code>
+          </fieldset>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default ContentBuilder;
