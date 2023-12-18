a76d66d96c224630e7e4de6040634cd426dce858
Sergey Solomentsev <tracer.nokia@gmail.com>
2023-12-18 00:30:28 -0500

    Using HashRouter.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import ContentBuilder from "./pages/ContentBuilder";
 
 export default function App() {
   return (
-    <BrowserRouter basename="hib-content-builder">
+    <HashRouter>
       <Routes>
         <Route index element={<Home />} />
         <Route path="builder" element={<ContentBuilder />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,13 @@
-import React from 'react';
-import logo from '../logo.svg';
+import React from "react";
+import logo from "../logo.svg";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Development of content builder for "Haskell Pocket Book" started.
-        </p>
+        <p>Development of content builder for "Haskell Pocket Book" started.</p>
         <a
           className="App-link"
           href="https://play.google.com/store/apps/details?id=com.WittyLion.haskell_is_beautiful"
@@ -17,6 +16,15 @@ function Home() {
         >
           Download in Play Market
         </a>
+        <a
+          className="App-link"
+          href="https://github.com/sergatgh/hib-content-builder"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Github
+        </a>
+        <Link to="/builder">Builder</Link>
       </header>
     </div>
   );
